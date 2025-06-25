3a91073ac0ce524da0d7b5cf2c8d499e17c6ba0b
ayato <2044taiga@gmail.com>
2025-06-25 18:30:37 +0900

    Update visp.rb

--- visp.rb
+++ visp.rb
@@ -1,8 +1,8 @@
 class Visp < Formula
   desc "A simple ffmpeg wrapper tool for Apple Silicon Mac, written in Common Lisp"
   homepage "https://github.com/ogrew/visp"
-  url "https://github.com/ogrew/visp/releases/download/v0.2.0/visp-macos-arm64.zip"
-  sha256 "cddd26dca4c915c3a0304ad50d665a991221b28b51c338680b565fb3e4069d7c"
+  url "https://github.com/ogrew/visp/releases/download/v0.3.0/visp-v0.3.0-macos-arm64.zip"
+  sha256 "07cd0e68d1aede1445750aceaf8ca2b98bb7501e3b91c521956b209f6d3735f5"
   license "MIT"
 
   if Hardware::CPU.intel?
@@ -10,6 +10,6 @@ class Visp < Formula
   end
 
   def install
-    bin.install "visp"
+    bin.install "visp-macos-arm64" => "visp"
   end
 end
